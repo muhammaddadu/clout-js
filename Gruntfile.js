a61eba03d78411b0e711873530f4d03793e788e8
Muhammad Dadu <muhammad@dadu.co>
2017-01-12 21:00:57 +0000

    moved CHANGELOG and added docgen

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,11 +8,24 @@ module.exports = function(grunt) {
     grunt.initConfig({
         mochaTest: {
             test: {
-                src: ['test/**/*_test.js']
+                src: ['test/*_test.js']
             }
-        }
+        },
+	    jsdoc : {
+	        dist : {
+	            src: ['bin/**/*.js', 'hooks/**/*.js', 'lib/**/*.js', 'test/**/*.js', 'index.js'],
+	            options: {
+	                destination: 'docs'
+	            }
+	        }
+	    }
     });
 
     grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-jsdoc');
+
 	grunt.registerTask('test', 'mochaTest');
+	grunt.registerTask('jsdoc', ['mochaTest', 'jsdoc']);
+
+	grunt.registerTask('defualt', ['mochaTest', 'jsdoc']);
 };
